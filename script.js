17a2f61dbab247ff7037828c215aec06fa056244
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-07-01 12:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+function isPerfectSquare(num) {
+  const sqrt = Math.sqrt(num);
+  return sqrt === Math.floor(sqrt);
+}
+
 function isFibonacci(num) {
-		// Write your code here
+  if (num < 0) return false;
+
+  const n1 = 5 * num * num + 4;
+  const n2 = 5 * num * num - 4;
+
+  return isPerfectSquare(n1) || isPerfectSquare(n2);
 }
 const input = parseInt(prompt("Enter a number"));
-alert(isFibonacci(input));
+alert(isFibonacci(input))
